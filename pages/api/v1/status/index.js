eba9ed9493ea21f83b18648ebb88fd5c23f2a07f
henemer <emerson@henning.com.br>
2026-01-28 19:10:29 +0000

    add `database.js` and `.env` files.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,11 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 AS sum;");
+  console.log(result.rows[0]);
   response
     .status(200)
     .json({ message: "alunos do curso.dev são pessoas acima da média" });
 }
 
 export default status;
-
