696ab9f99f76e9d9ef554959102a45714338092f
Emerson Henning <emerson@henning.com.br>
2026-02-16 13:18:58 -0300

    fix  endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,6 +3,10 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({ error: `Method "${request.method}" Not Allowed` });
+  }
   let dbClient;
   try {
     dbClient = await database.getNewClient();
@@ -32,10 +36,9 @@ export default async function migrations(request, response) {
       return response.status(200).json(migratedMigrations);
   
     }
-    return response.status(405).end(); // Method not allowed
   } catch (error) {
     console.error("Migration error:", error);
-    return response.status(500).json({ error: "Internal Server Error" });
+    throw new error;
   }
   finally {
     if (dbClient) {
