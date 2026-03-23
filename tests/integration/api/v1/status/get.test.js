19cc69a20a243c9ef8e46f2330c1bf0098e9443d
Emerson Henning <emerson@henning.com.br>
2026-03-23 16:19:37 -0300

    adds  Github actions

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,7 +2,7 @@ import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-})
+});
 
 test("GET  to /api/v1/status shoul retorn 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
@@ -17,5 +17,3 @@ test("GET  to /api/v1/status shoul retorn 200", async () => {
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
 });
-
-
--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,25 +1,23 @@
-import retry from "async-retry"
+import retry from "async-retry";
 
 async function waitForAllServices() {
-    await waitforWebServer();
+  await waitforWebServer();
 
-    async function waitforWebServer() {
-        return retry(fechStatusPage, {
-            retries: 100,
-            maxTimeout: 1000,
-        })
+  async function waitforWebServer() {
+    return retry(fechStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
 
-        async function fechStatusPage() {
-            const response = await fetch("http://localhost:3000/api/v1/status");
-            if(response.status !== 200) 
-            {
-                throw Error();
-                
-            }
-        }
+    async function fechStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      if (response.status !== 200) {
+        throw Error();
+      }
     }
+  }
 }
 
 export default {
-    waitForAllServices,
-}
+  waitForAllServices,
+};
